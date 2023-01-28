4762c24e4f2cf0d3c7dce37823f9045f3cb26c7c
FranciscoCPP <francisco.cpp@gmail.com>
2023-01-28 13:00:45 -0300

    modificacion

--- src/principal/tarea6/Tarea6.java
+++ src/principal/tarea6/Tarea6.java
@@ -7,13 +7,15 @@ public class Tarea6 {
 
     public static void main(String[] args) {
 
-        System.out.println("El IVA de 1000 es el 19% =" + calcularIva(1000,19));
+        double valor = 1000;
+        int iva = 19;
+        System.out.println("El IVA de " + valor +" es el "+iva+"%, por lo tanto el precio es de =" + calcularIva(valor,iva));
 
 
     }
 
-    public static double calcularIva(int valorSinIva, double porcentajeIva){
-        return valorSinIva * (porcentajeIva/100);
+    public static double calcularIva(double valorSinIva, double porcentajeIva){
+        return (valorSinIva * (porcentajeIva/100)) + valorSinIva;
     }
 
 }
